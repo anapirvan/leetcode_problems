ed21f76654318ff62ff2a04dc5998b12316f8ec0
ana.pirvan <anampirvan05@gmail.com>
2025-10-09 23:23:39 +0300

    Group anagrams

---
+++ lc_49/code.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs)
+    {
+        unordered_map<string, vector<string>> m;
+        for (string s : strs)
+        {
+            string key = s;
+            sort(key.begin(), key.end());
+            m[key].push_back(s);
+        }
+        vector<vector<string>> res;
+        for (auto& it : m)
+            res.push_back(it.second);
+        return res;
+    }
+};
